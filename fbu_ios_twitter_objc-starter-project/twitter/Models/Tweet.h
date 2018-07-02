4430a60517f3f388e0db901bf33efd1ed2bdf026
Gisele Phalo <zellebelle@fb.com>
2018-07-02 14:27:46 -0700

    here

---
+++ fbu_ios_twitter_objc-starter-project/twitter/Models/Tweet.h
@@ -0,0 +1,26 @@
+//
+//  Tweet.h
+//  twitter
+//
+//  Created by Gisele Phalo on 7/2/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tweet : NSObject
+
+// MARK: Properties
+@property (nonatomic, strong) NSString *idStr; // For favoriting, retweeting & replying
+@property (strong, nonatomic) NSString *text; // Text content of tweet
+@property (nonatomic) int favoriteCount; // Update favorite count label
+@property (nonatomic) BOOL favorited; // Configure favorite button
+@property (nonatomic) int retweetCount; // Update favorite count label
+@property (nonatomic) BOOL retweeted; // Configure retweet button
+@property (strong, nonatomic) User *user; // Contains name, screenname, etc. of tweet author
+@property (strong, nonatomic) NSString *createdAtString; // Display date
+
+// For Retweets
+@property (strong, nonatomic) User *retweetedByUser;  // user who retweeted if tweet is retweet
+
+@end
